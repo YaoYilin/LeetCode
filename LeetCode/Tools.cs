95f9cabd2f4185fefb301e2ae3dcd8fb8ec9b8d5
unknown <dn.momo110@immomo.com>
2023-09-28 16:32:17 +0800

    update a tool for refresh readme.md

--- LeetCode/Tools.cs
+++ LeetCode/Tools.cs
@@ -1,13 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace LeetCode
 {
     public static class Tools
     {
+        public static void UpdateReadMe()
+        {
+            string basePath = "https://github.com/YaoYilin/LeetCode/blob/master/LeetCode/";
+            try
+            {
+                string readMe = Path.Combine(Environment.CurrentDirectory, "../../../README.md");
+                string[] lines = File.ReadAllLines(readMe);
+                string[] easy = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "../../Easy.cs"));
+                string[] medium = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "../../Medium.cs"));
+                string[] hard = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "../../Hard.cs"));
+
+
+                string[] fileLines = null;
+                string fileName = null;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string line = lines[i];
+                    string str = line.Trim();
+                    if (str.StartsWith("###"))
+                    {
+                        if (str.EndsWith("Easy"))
+                        {
+                            fileLines = easy;
+                            fileName = "Easy.cs";
+                        }
+                        else if (str.EndsWith("Medium"))
+                        {
+                            fileLines = medium;
+                            fileName = "Medium.cs";
+                        }
+                        else if (str.EndsWith("Hard"))
+                        {
+                            fileLines = hard;
+                            fileName = "Hard.cs";
+                        }
+                    }
+
+                    if (fileLines != null)
+                    {
+                        if (str.StartsWith("*"))
+                        {
+                            Regex regex = new Regex(@"(?<=[\[])[^]]*");
+                            MatchCollection collection = regex.Matches(str);
+                            if (collection.Count > 0)
+                            {
+                                string name = collection[0].Value;
+                                int l = GetCodeLine(fileLines, name);
+                                
+                                Regex linkRegex = new Regex(@"(([^()]+))");
+                                var match = linkRegex.Matches(regex.Replace(str, ""));
+                                if (match.Count > 1)
+                                {
+                                    lines[i] = $"* [{name}]({match[1].Value}) (<u> [答案跳转]({basePath}{fileName}#L{l}) </u>)";
+                                }
+                            }
+                        }
+                    }
+                }
+                File.WriteAllLines(readMe, lines, Encoding.UTF8);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static int GetCodeLine(string[] group, string name)
+        {
+            for (int i = 0; i < group.Length; i++)
+            {
+                if (group[i].Contains(name))
+                {
+                    return i + 1;
+                }
+            }
+
+            return 1;
+        }
+        
         public static char ToUpper(char c)
         {
             if('a' <= c && c <= 'z')
